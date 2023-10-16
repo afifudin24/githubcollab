eb560adb4713353e208cc70871fb6767c4b9455e
YeNiZhiao <afifrider507@gmail.com>
2023-10-16 14:53:00 +0700

    Ngetes lagi

--- app/src/main/java/com/example/wo_macro/BerandActivity.kt
+++ app/src/main/java/com/example/wo_macro/BerandActivity.kt
@@ -7,5 +7,6 @@ class BerandActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_berand)
+        val oke = "Lah"
     }
 }
